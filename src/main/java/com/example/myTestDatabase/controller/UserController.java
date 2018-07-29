b9c4b6a5c65ae4c1a8c500bb53a26863fb9b9219
ivana <ivana.ristova95@yahoo.com>
2018-07-29 12:08:15 +0100

    changes in controller

--- src/main/java/com/example/myTestDatabase/controller/UserController.java
+++ src/main/java/com/example/myTestDatabase/controller/UserController.java
@@ -7,11 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
-    // Private fields
 
     private final UserRepository userRepository;
 
@@ -19,9 +18,6 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    /**
-     * GET /create  --> Create a new user and save it in the database.
-     */
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@Valid @RequestBody UserCreateRequest request) {
@@ -29,9 +25,6 @@ public class UserController {
         return userRepository.save(user);
     }
 
-    /**
-     * GET /delete  --> Delete the user having the passed id.
-     */
     @DeleteMapping("/delete/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String email) {
@@ -39,39 +32,19 @@ public class UserController {
             userRepository.delete(user);
     }
 
-    /**
-     * GET /get-by-email  --> Return the id for the user having the passed
-     * email.
-     */
-    @RequestMapping("/get-by-email")
-    @ResponseBody
-    public String getByEmail(String email) {
-        String userId = "";
-        try {
-            User user = userRepository.findByEmail(email);
-            userId = String.valueOf(user.getId());
-        } catch (Exception ex) {
-            return "User not found";
-        }
-        return "The user id is: " + userId;
+    @GetMapping("/{email}")
+    public User getByEmail(@PathVariable String email) {
+            User user =  userRepository.findByEmail(email);
+        System.out.println(user);
+        return user;
     }
 
-    /**
-     * GET /update  --> Update the email and the name for the user in the
-     * database having the passed id.
-     */
-    @RequestMapping("/update")
-    @ResponseBody
-    public String updateUser(long id, String email, String name) {
-        try {
-            User user = userRepository.findById(id).orElse(null);
-            user.setEmail(email);
-            user.setName(name);
-            userRepository.save(user);
-        } catch (Exception ex) {
-            return "Error updating the user: " + ex.toString();
-        }
-        return "User succesfully updated!";
+    @PutMapping("/update/{email}")
+    public User updateUser(@PathVariable String email, @Valid @RequestBody UserCreateRequest request) {
+            User user = userRepository.findByEmail(email);
+            user.setEmail(request.email);
+            user.setName(request.name);
+            return userRepository.save(user);
     }
 
 
--- src/main/java/com/example/myTestDatabase/repository/UserRepository.java
+++ src/main/java/com/example/myTestDatabase/repository/UserRepository.java
@@ -4,15 +4,8 @@ import com.example.myTestDatabase.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    /**
-     * This method will find an User instance in the database by its email.
-     * Note that this method is not implemented and its working code will be
-     * automagically generated from its signature by Spring Data JPA.
-     */
     User findByEmail(String email);
 }
